38ea8ff616ee4395b5792e29728ef2e6d2811556
Oleg <89131899822f@gmail.com>
2021-08-17 19:38:52 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//alert("helo");
+//onst result = confirm("are you here?") ;
+//console.log(result);
+/*const answer = prompt("Вам есть уже 18?", "18");
+console.log(answer);*/
+
+const answers = [];
+
+answers[0] = prompt("Введите ваше имя ?");
+answers[1] = prompt("ВВедите вашу фамилию ?");
+answers[2] = prompt("введите ваш возраст ?");
+
+console.log(answers);
